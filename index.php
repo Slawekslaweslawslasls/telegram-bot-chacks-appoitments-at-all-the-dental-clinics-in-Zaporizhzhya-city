d49650bbbd866e02d5a32b22529ff50fa052a1a7
Slawek <35809677+Slawekslaweslawslasls@users.noreply.github.com>
2019-04-17 21:14:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 ﻿<?php
 /*Done! Congratulations on your new bot. You will find it at t.me/ya_v_zp_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
 
-Use this token to access the HTTP API:
-781684063:AAFR2Z3u7xEqUxYO1gONvg8XSUysDenqGBY
-Keep your token secure and store it safely, it can be used by anyone to control your bot.
 
 For a description of the Bot API, see this page: https://core.telegram.org/bots/api
 */
