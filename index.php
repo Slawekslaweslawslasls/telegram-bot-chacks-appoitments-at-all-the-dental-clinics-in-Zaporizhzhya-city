87da9de0ffa2bb087c030bcebf8376dc0af9fc03
Slawek <yellowpostman@gmail.com>
2019-04-23 08:45:06 +0200

    development is keeps

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 ﻿<?php
 /*Done! Congratulations on your new bot. You will find it at t.me/ya_v_zp_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
 
-Use this token to access the HTTP API:
-781684063:AAFR2Z3u7xEqUxYO1gONvg8XSUysDenqGBY
-Keep your token secure and store it safely, it can be used by anyone to control your bot.
-
 For a description of the Bot API, see this page: https://core.telegram.org/bots/api
 */
 
@@ -42,46 +38,126 @@ function sendMessage(){
 
 //sendMessage();
 
-//function getUpdates(){
-$updates =json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
-file_put_contents(__DIR__.'/log.txt', file_get_contents('php://input'), FILE_APPEND);
-
+function ToLogFile($fileName, $body){
+	if (isset($fileName) ){
+	 	file_put_contents(__DIR__.'/'.$fileName, $body.PHP_EOL.PHP_EOL, FILE_APPEND);
+	 	//return $output;
+	}
+}
 
 
 
 function sendRequest($params,$replyMarkup){
 
 	if(!empty($params)){
-	return	
+		
 		 //json_decode(file_get_contents(BASE_URL.'sendMessage?'.http_build_query($params)),JSON_OBJECT_AS_ARRAY);
 		 
-
-    file_put_contents(__DIR__.'/log_responses.txt',  file_get_contents(BASE_URL.'sendMessage?'.http_build_query($params).'&reply_markup=' . $replyMarkup), FILE_APPEND);
+	$raw_query=file_get_contents($url_query=BASE_URL.'sendMessage?'.http_build_query($params).'&reply_markup=' . $replyMarkup);
+    //file_put_contents(__DIR__.'/log_array_responses.txt', file_get_contents('php://input'), FILE_APPEND);
+    ToLogFile('log_responses.txt',urldecode ($raw_query));
+    ToLogFile('log_url.txt',$url_query);
+    
+	//file_put_contents(__DIR__.'/log_input.txt', file_get_contents('php://input'), FILE_APPEND);
+    //file_put_contents(__DIR__.'/log_responses.txt',  $raw_query, FILE_APPEND);
+    //file_put_contents(__DIR__.'/log_url.txt', $query);
+    
     //echo BASE_URL.'sendMessage?'.http_build_query($params).'&reply_markup=' . $replyMarkup;
+    
 	}else{
 		printf('check parameters');
 	}
 
 }
 
-$chat_id=$updates['message']['chat']['id'];
 
-	$inline_button1 = array("text"=>"Google url","url"=>"http://google.com");
-    $inline_button2 = array("text"=>"work plz","callback_data"=>'/plz');
-    $inline_keyboard = [[$inline_button1,$inline_button2]];
-    $keyboard=array("inline_keyboard"=>$inline_keyboard,"resize_keyboard" => true);
-    $replyMarkup = json_encode($keyboard, JSON_UNESCAPED_UNICODE); 
+
+$GLOBALS['updates']=json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
+ToLogFile('log_input_stream.txt', file_get_contents('php://input'));
+//$GLOBALS ['chat_id']=$updates['message']['chat']['id'];
+//ToLogFile('log_id.txt', $chat_id);
+$GLOBALS ['text'] = $GLOBALS['updates']["message"]["text"];
+//$GLOBALS ['callback_query']=$GLOBALS['updates']["callback_query"]['data'];
+//ToLogFile('callback_query', $GLOBALS ['callback_query']);
+
+
+
+	//$inline_button1 = array("text"=>"Google url","url"=>"http://google.com");
+    //$inline_button2 = array("text"=>"work plz","callback_data"=>'/plz');
+    //$inline_keyboard = [[$inline_button1,$inline_button2]];
+    //$keyboard=array("inline_keyboard"=>$inline_keyboard,"resize_keyboard" => true);
+    //$replyMarkup = json_encode($keyboard, JSON_UNESCAPED_UNICODE); 
 
 	
- 	$keyboard_test=array("inline_keyboard"=>[domThreeHandler('//*[contains(@class,"article-intro")][p]')],'one_time_keyboard' => true, "resize_keyboard" => true, 'parse_mode'=>HTML,);
- 	$replyMarkupTest=json_encode($keyboard_test, JSON_UNESCAPED_UNICODE);
+ 	//$keyboard_test=array("inline_keyboard"=>[{"text":" Нажми","callback_data":"\/help"}],'resize_keyboard' => true);
+ 	//$replyMarkupTest=json_encode($keyboard_test, JSON_UNESCAPED_UNICODE);
 
+function helper_check_id($id){
+		return is_numeric($id);
+}
+
+function messageIterator($xpathQuery, $url_parse, $chat_id){
+
+		
+	if(!empty($xpathQuery)&&!empty($url_parse)&&!empty($chat_id)&&helper_check_id($chat_id)){
+
+		$domObj=domTreeHandler($xpathQuery, $url_parse);
+
+		for ($i=1; $i < count($domObj); $i++) { 
+			$replyMarkupInline=array("inline_keyboard"=>array([["text"=>"Выбрать","callback_data"=>$i]]),'resize_keyboard' => true);
+			$replyMarkupTest=json_encode($replyMarkupInline, JSON_UNESCAPED_UNICODE);
+			sendRequest(['chat_id'=>$chat_id,'text'=>$domObj[$i]], $replyMarkupTest);
+			//$updates2=json_decode(file_get_contents('php://input'), JSON_OBJECT_AS_ARRAY);
+		    ToLogFile('log_input.txt', file_get_contents('php://input'));
+		}
+
+	}else{
+		$warning_text='Упс, что-то пошло не так';
+		$replyMarkupInline=array("inline_keyboard"=>array([["text"=>"Сообщить","url"=>'mailto:slawek@slawek.dev']]),'resize_keyboard' => true);
+		$replyMarkupTest=json_encode($replyMarkupInline, JSON_UNESCAPED_UNICODE);
+
+		$warning_id=(!helper_check_id($GLOBALS['updates']['callback_query']['message']['chat']['id']))?
+		$GLOBALS['updates']['message']['chat']['id']:
+		$GLOBALS['updates']['callback_query']['message']['chat']['id'];
+		sendRequest(['chat_id'=>$warning_id,'text'=>$warning_text], $replyMarkupTest);
+
+		ToLogFile('log_failed_input.txt', $warning_id);
+	}
+}
+echo ";ldd";
+switch ($GLOBALS['text']) {
+    case '/start':
+     	messageIterator('//*[contains(@class,"article-intro")][p]', 'http://health.zp.ua/stomatolohiya', $GLOBALS['updates']['message']['chat']['id']);
+		break;
+    case '\/help':
+     	echo "help";
+     	break;
+}
+
+if (!empty($GLOBALS['updates']['callback_query']['data'])){
+	 $callback=$GLOBALS['updates']['callback_query']['data'];
+	 $callback_id=$GLOBALS['updates']['callback_query']['message']['chat']['id'];
+	switch ($callback) {
+	    case ($callback>0):
+	    	
+	     	$deep_1=domTreeHandler('(//*[contains(@class,"readmore")]/a/@href)['.$callback.']', 'http://health.zp.ua/stomatolohiya');
+	     	$deep_2=domTreeHandler('(//a[contains(@class,"btn-secondary4")]/@href)[1]' ,$temp1='health.zp.ua/stomatolohiya'.$deep_1[0]);
+	     	//$deep_3=domTreeHandler('//div[contains(@class,"button-4-gl")]/a/@href', $deep_2);//get doctors appointments
+	     	//messageIterator('//div[contains(@class,"button-4-gl")]/a/@href', $deep_2[0], $callback_id);
+	     	ToLogFile('log_jyst temportest.txt', json_encode($deep_1).'---'.$deep_2[0].'---'.$callback.'---'.$callback_id.'=----'.$temp1);
+			break;
+	    case '\/help':
+	     	echo "help";
+	     	break;
+	}
+}
  	//var_dump(json_encode($keyboard_test));
- 	var_dump($replyMarkup);
+ 	//var_dump($replyMarkup);
  	//var_dump($keyboard);
- 	var_dump($replyMarkupTest);
-	sendRequest(['chat_id'=>$chat_id,'text'=>$init_msg], $replyMarkupTest);
+ 	//var_dump($replyMarkupTest);
+	//sendRequest(['chat_id'=>$chat_id,'text'=>domTreeHandler('//*[contains(@class,"article-intro")][p]')], $replyMarkupTest);
 //echo '<br>'.$i++.' - '.$nodes_iterator_announce_title=$value->nodeValue; 
+unset($updates);
 ?>
 
 
--- parser.php
+++ parser.php
@@ -15,23 +15,28 @@ set_time_limit(0);
 		//var_dump($xpath);
 		
 
-function domThreeHandler($xpathquery){
+function domTreeHandler($xpathquery, $url_parse){
 		//var_dump($xpath);
 		$i=0;
 		$j=0;
-		$url="http://health.zp.ua/stomatolohiya";
-        $curl = curl_init($url);
+		//$url_parse="sp3.health.zp.ua/zapys-do-likarya-st1";
+		
+        $curl = curl_init($url_parse);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
 		curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 0); 
 		$html = curl_exec($curl);
 		$dom = new DOMDocument('1.0', 'UTF-8');
 		@$dom->loadHTML($html);
-		 $xpath = new DOMXPath($dom);
-		//$elements = $xpath->query("//*[contains(@class,'article-intro')][p]");
+		$xpath = new DOMXPath($dom);
+		//$xpathquery = '(//div[contains(@class,"button-4-gl")]/a/@href)';
+
+		//*[count(BBB)=2]  [@class="class"]
 		$elements = $xpath->query($xpathquery);
+
 		if(!empty($xpathquery)){
 		foreach ($elements as $value) {
-			$nodes_inline_keyboard[]= array('text'=>preg_replace("/[\n\r\t]/", "", $value->nodeValue),'callback_data'=>'/help'); 
+			//$nodes_inline_keyboard[]= array(['text'=>preg_replace("/[\n\r\t]/", "", $value->nodeValue),'callback_data'=>'/help']); 
+			$nodes_inline_keyboard[]= preg_replace("/[\n\r\t]/", "", $value->nodeValue); 
 			//var_dump($value->nodeName);
 			
 			}	
@@ -40,9 +45,8 @@ function domThreeHandler($xpathquery){
 		}
 
 		return $nodes_inline_keyboard;
-//var_dump($nodes_inline_keyboard);
 }
-//var_dump(domThreeHandler('//*[contains(@class,"article-intro")][p]'));
+//var_dump(domTreeHandler('void',"void"));
 
 
 ?>
